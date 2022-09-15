d31b7cf6b605de1c2f31ba2981e80392295397b6
Vandim87 <107519365+Vandim87@users.noreply.github.com>
2022-09-15 14:09:50 -0300

    1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function abrirmenu(){
     let BarraMenuAberto = document.getElementById ('barra-menu')
     if (BarraMenuAberto.style.width == "0px"){
-        BarraMenuAberto.style.width = "180px";
+        BarraMenuAberto.style.width = "30vh";
     } else {BarraMenuAberto.style.width = "0px"}
 }
